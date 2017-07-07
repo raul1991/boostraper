e987389b71df1f008c6b76ea7ff85233ff10c386
erahbaw <rahul.bawa@ericsson.com>
2017-07-07 08:55:25 +0530

    Added ui-router
    Closes #5

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
-angular.module("bootstrapper",[])
+angular.module("bootstrapper",['ui.router'])
 .controller("IndexPageController", ['$scope', function($scope) {
     $scope.message = "Hello, World!!!";
-}]);
+}])
+.config(function($qProvider, $stateProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+    $stateProvider
+    .state('home', {
+        url: "/home",
+        templateUrl: '/app/partials/home.html'
+    });
+});
