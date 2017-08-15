f6ded546213befd5c026a1ec6c671a01cad38ba2
cafebabe1991 <supremeanitabawa@gmail.com>
2017-08-15 15:09:12 +0530

    Updated README.md
    Corrected the format.

--- server/server.py
+++ server/server.py
@@ -1,10 +1,21 @@
 import flask
 from flask import Flask, render_template
+from flask.ext.mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'heroku_w1hjpl69'
+app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://heroku_w1hjpl69:llmljj3stm2f3ei1v6906l83j2@ds123084.mlab.com:23084/heroku_w1hjpl69'
+
+db = MongoAlchemy(app)
+
+class Item(db.Document):
+    name = db.StringField()
+    quantity = db.IntField()
 
 @app.route('/')
 def hello_world():
+        item = Item(name='Rahul', quantity=2)
+        item.save()
         return render_template('index.html')
 
 @app.route('/home')
