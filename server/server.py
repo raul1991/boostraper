31ed054d32eab03a476f1b87c074f3298c624480
raul1991 <supremeanitabawa@gmail.com>
2017-09-03 18:09:39 +0530

    Added support for mongo orm using MongoAlchemy.
    Refs:
    mlab.com
    heroku -> https://devcenter.heroku.com/articles/mongolab#using-mlab-s-admin-tools-for-mongodb
    MongoAlchemy -> https://pythonhosted.org/Flask-MongoAlchemy/

--- server/server.py
+++ server/server.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 from flask.ext.mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'heroku_w1hjpl69'
-app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://heroku_w1hjpl69:llmljj3stm2f3ei1v6906l83j2@ds123084.mlab.com:23084/heroku_w1hjpl69'
+app.config['MONGOALCHEMY_DATABASE'] = '<your-db-name>'
+app.config['MONGOALCHEMY_CONNECTION_STRING'] = '<your-connection-string>'
 
 db = MongoAlchemy(app)
 
